a86493941e7d906856f66f013410767591431a74
corentin.dupaigne <declan.lowee@gmail.com>
2024-11-14 13:53:43 +0100

    feat: correct manage workspaces script and add renumber workspaces script

--- i3/scripts/manage_workspaces.py
+++ i3/scripts/manage_workspaces.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import i3ipc
 
 def adjust_layout(i3, event):
@@ -9,10 +11,10 @@ def adjust_layout(i3, event):
     workspaces = sorted(tree.workspaces(), key=lambda ws: ws.num)
     for i in range(1, len(workspaces)): # start at 1 to exclude workspace 1
         if not workspaces[i].leaves:
-            for j in range(i+1, len(workspaces)):
-                i3.command(f"workspace {j}.name; move workspace to workspace {j-1}.name")
+            i3.command(f"workspace {i}.name; move workspace to output null")
         
 if __name__ == "__main__":
     i3 = i3ipc.Connection()
+    print(i3.get_tree().workspace())
     # only call the function when just moved from an empty desktop to avoid autoclosing a just opened workspace
     i3.on("workspace::focus", adjust_layout)
