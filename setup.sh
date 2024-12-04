14094b564f1ab8645039225fa1e4ea802174e3fc
corentin.dupaigne <declan.lowee@gmail.com>
2024-12-04 21:09:39 +0100

    feat: flameshot config and refactored a few files

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-source ~/.config/.env
-
 # Install packages
-$ROOT_PATH/scripts/install_packages.sh
+$HOME/.config/scripts/install_packages.sh
 
 # Set config
-$ROOT_PATH/scripts/set_config.sh
+$HOME/.config/scripts/set_config.sh
 
 # Set fonts
-$ROOT_PATH/scripts/set_fonts.sh
+$HOME/.config/scripts/set_fonts.sh
 
 # Setup rofi (Not working from here, execute it from rofi folder)
-# $ROOT_PATH/rofi/setup.sh
+# $HOME/.config/rofi/setup.sh
